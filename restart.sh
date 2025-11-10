8ea944b12748d45800639aad29e66dcbee231811
deklan400 <fadlan400@gmail.com>
2025-11-10 09:57:51 +0700

    Update restart script to restart gensyn service

--- restart.sh
+++ restart.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-echo "[*] Restarting RL-Swarm service..."
+echo "[*] Restart gensyn systemd service"
 sudo systemctl restart gensyn
 sleep 2
 sudo systemctl status gensyn --no-pager
