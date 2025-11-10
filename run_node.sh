52f775dc7423c2155257bf3ed8d89388506373f5
deklan400 <fadlan400@gmail.com>
2025-11-10 08:58:08 +0700

    Refactor run_node.sh to simplify execution

--- run_node.sh
+++ run_node.sh
@@ -3,8 +3,4 @@ set -e
 
 cd ~/rl-swarm || { echo "rl-swarm folder not found"; exit 1; }
 
-echo "[*] Starting RL-Swarm..."
-screen -S swarm -dm bash -c "
-    docker compose run --rm --build -Pit swarm-cpu
-"
-echo "[+] RL-Swarm started in screen session: swarm"
+exec /usr/bin/docker compose run --rm -Pit swarm-cpu
