947c21c9fef6b7f3e12de953375d6f3a0ff3ad58
deklan400 <fadlan400@gmail.com>
2025-11-10 10:31:38 +0700

    Update run_node.sh

--- run_node.sh
+++ run_node.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-# change to rl-swarm dir (same as RL_DIR in install.sh)
 cd /home/gensyn/rl_swarm || { echo "rl-swarm folder not found"; exit 1; }
 
-# Run docker compose (exec so PID managed by systemd)
+echo "[*] Starting swarm-cpu docker..."
 exec /usr/bin/docker compose run --rm -Pit swarm-cpu
